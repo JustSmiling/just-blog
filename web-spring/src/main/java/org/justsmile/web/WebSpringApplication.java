433af61c7b19628bfcde6a7083a08e6de1fbc0ac
유정근 <justsmile@miridih.com>
2022-11-05 22:09:01 +0900

    안녕 세계

--- web-spring/src/main/java/org/justsmile/web/WebSpringApplication.java
+++ web-spring/src/main/java/org/justsmile/web/WebSpringApplication.java
@@ -1,5 +1,13 @@
+package org.justsmile.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class WebSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringApplication.class, args);
+    }
 }
--- web-spring/src/main/java/org/justsmile/web/view/HelloController.java
+++ web-spring/src/main/java/org/justsmile/web/view/HelloController.java
@@ -1,2 +1,13 @@
-package org.justsmile.web.view;public class HelloController {
+package org.justsmile.web.view;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/index")
+    String hello() {
+        return "HelloWorld";
+    }
 }
